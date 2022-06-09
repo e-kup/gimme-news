d06498d012935a68a8e0922e24f6ae8a352b91be
Ewa Kupczynska <ewa.kupczynska@schibsted.com>
2022-06-09 12:06:42 +0200

    feat: add style placeholders

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,31 @@
-import { LiveReload } from '@remix-run/react';
+import { LiveReload, Links, Outlet } from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/node';
 
-export default function App() {
+import globalStylesUrl from './styles/global.css';
+
+const App = () => {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
-        Hello world
+        <Outlet />
         <LiveReload />
       </body>
     </html>
   );
-}
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStylesUrl,
+    },
+  ];
+};
+
+export default App;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,14 @@
 import { VFC } from 'react';
+import type { LinksFunction } from '@remix-run/node';
+
+import stylesUrl from '~/styles/index.css';
 
 const Test: VFC = () => {
   return <div>test</div>;
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
 export default Test;
