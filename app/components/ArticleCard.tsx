3ce68b0d71a754f84f5f555dd5c63b8d1e854bdb
Ewa Kupczynska <ewa.kupczynska@schibsted.com>
2022-06-13 15:27:30 +0200

    refactor: extract icons into components

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -1,5 +1,7 @@
 import { useState, FC } from 'react';
 import LoginModalTrigger from '~/components/LoginModalTrigger';
+import BookmarkIcon from '~/components/icons/Bookmark';
+import StoredBookmarkIcon from '~/components/icons/StoredBookmark';
 
 interface CardProps {
   url: string;
@@ -9,40 +11,6 @@ interface CardProps {
   publicationDate: string;
   userId?: string;
 }
-
-const bookmark = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-    />
-  </svg>
-);
-
-const storedBookmark = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="currentColor"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-    />
-  </svg>
-);
 
 const ArticleCard: FC<CardProps> = ({
   url,
@@ -80,8 +48,12 @@ const ArticleCard: FC<CardProps> = ({
                     checked={isBookmarked}
                     onChange={onBookmarkChange}
                   />
-                  <div className="swap-on">{storedBookmark}</div>
-                  <div className="swap-off">{bookmark}</div>
+                  <div className="swap-on">
+                    <StoredBookmarkIcon />
+                  </div>
+                  <div className="swap-off">
+                    <BookmarkIcon />
+                  </div>
                 </label>
               </div>
             ) : (
@@ -90,7 +62,9 @@ const ArticleCard: FC<CardProps> = ({
                   className="tooltip tooltip-left"
                   data-tip="add to bookmarks"
                 >
-                  <div className="swap-off">{bookmark}</div>
+                  <div className="swap-off">
+                    <BookmarkIcon />
+                  </div>
                 </div>
               </LoginModalTrigger>
             )}
---
+++ app/components/icons/Bookmark.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+
+const StoredBookmarkIcon: FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+      />
+    </svg>
+  );
+};
+
+export default StoredBookmarkIcon;
