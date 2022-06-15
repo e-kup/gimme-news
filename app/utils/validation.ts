8a61e18e3eb881248535ba01ca8a120a5cad7017
Ewa Kupczynska <ewa.kupczynska@schibsted.com>
2022-06-15 15:41:00 +0200

    feat: increase limit of characters in login form

--- app/utils/validation.ts
+++ app/utils/validation.ts
@@ -2,8 +2,8 @@ export const validateUsername = (username: unknown) => {
   if (typeof username !== 'string' || username.length < 3) {
     return `Usernames must be at least 3 characters long`;
   }
-  if (username.length > 30) {
-    return `Usernames must be less than 30 characters long`;
+  if (username.length > 50) {
+    return `Usernames must be less than 50 characters long`;
   }
 };
 
@@ -11,7 +11,7 @@ export const validatePassword = (password: unknown) => {
   if (typeof password !== 'string' || password.length < 6) {
     return `Passwords must be at least 6 characters long`;
   }
-  if (password.length > 30) {
-    return `Passwords must be less than 30 characters long`;
+  if (password.length > 50) {
+    return `Passwords must be less than 50 characters long`;
   }
 };
